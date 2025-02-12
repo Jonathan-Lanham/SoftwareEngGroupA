be4b9bf3009b24e7982e93e9725663e0cf63e450
Samuel Markus <sjmark4@gmail.com>
2025-02-12 18:49:14 -0500

    Update sketch.js

--- main/sketch.js
+++ main/sketch.js
@@ -26,8 +26,40 @@ class LogicGate {
     }
 }
 
+// Button parent class, gonna need to add a mousee event to this
+class Button
+{
+    constructor(x, y)
+    {
+        // Coordinates for where the button will be placed
+        this.x = x; 
+        this.y = y;  
+    }
+    // Action: Overrided by child class perform an action upon the button being pressed
+    action() {};
+
+    // Display: Displays the button on the screen
+    display()
+    {
+        rect(this.x, this.y, gateSizeWidth, gateSizeHeight, 10);
+        textAlign(CENTER, CENTER);
+        text("BUTTON", this.x + 30, this.y + 20);
+    }
+}
+
+const createButton = (text = 'No text') =>
+{
+    const button = document.createElement('button');
+    button.innerText = text;
+    document.body.appendChild(button);
+    return button;
+}
+
 //Will be used to store all gates currently on screen. Work in progress.
 let gates = [];
+
+// List for buttons
+let buttons = [];
 
 //Initial setup; Only runs once
 // "It can be used to set default values for your project."
@@ -47,6 +79,9 @@ function setup() {
     //Set the background color to that intriguing shade of blue.
     background('#4287f5');
 
+    // Place buttons on screen
+    buttons.push(new Button(500, 500));
+
     //Initialize a gate? work in progress, will need to reference a "level database" for initializing "puzzles"
     gates.push(new LogicGate(200, 350, "AND"));
     //gates.push(new LogicGate(200, 300, "OR"));
@@ -58,6 +93,12 @@ function draw() {
 
     //If you want to see something fun, comment this out
     background('#4287f5');
+
+    // For each loop that displays each button in buttons
+    for (let button of buttons)
+    {
+        button.display();
+    }
 
     //Loops over every gate, and updates it
     for (let gate of gates) {
